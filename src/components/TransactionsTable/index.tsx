f37885d688378bbb07bf0947e81b49f673848bd6
Home <osxteste@MacBook-de-Home-2.local>
2021-10-18 11:53:54 -0300

    Formatting values

--- src/components/TransactionsTable/index.tsx
+++ src/components/TransactionsTable/index.tsx
@@ -36,9 +36,12 @@ export function TransactionsTable() {
           {transactions.map(transaction => (
             <tr key={transaction.id}>
               <td>{transaction.title}</td>
-              <td className={transaction.type}>{transaction.amount}</td>
+              <td className={transaction.type}>{new Intl.NumberFormat('pt_BR', {
+                style: 'currency',
+                currency:'BRL',
+              }).format(transaction.amount)}</td>
               <td>{transaction.category}</td>
-              <td>{transaction.createdAt}</td>
+              <td>{new Intl.DateTimeFormat('pt_BR').format(new Date(transaction.createdAt))}</td>
             </tr>
           ))}
         </tbody>
